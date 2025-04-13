a68b30a0f1fb7de13ad61253ff0b57f0651d6997
SebastianArevalo-19 <sebastianarevalom19@gmail.com>
2025-04-12 19:57:30 -0500

    se añadio

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,50 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import  java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+         Votacion votacion = new Votacion();
+         Scanner scanner = new Scanner(System.in);
 
+        while(true){
+            System.out.println("Menu:");
+            System.out.println("1.Votar");
+            System.out.println("2.Calcular el costo de la campaña");
+            System.out.println("3.Vaciar urnas");
+            System.out.println("4.Saber los resultados");
+            System.out.println("5.Salir");
+
+            System.out.println("Ingrese su opcion");
+            int opcion = scanner.nextInt();
+            switch (opcion){
+                case 1:
+                    System.out.println("Seleccione un candidato:");
+                    for(int i=0; i< votacion.candidatoes.size(); i++){
+                        System.out.println((i+1) + "." + votacion.candidatoes.get(i).getNombre());
+                    }
+                    int candidatoSeleccionado = scanner.nextInt()  - 1;
+                    System.out.println("Seleccione el medio de influencia:");
+                    System.out.println("1.Internet");
+                    System.out.println("2.Radio");
+                    System.out.println("3.Television");
+                    int medioSeleccionado = scanner.nextInt();
+                    votacion.votar(candidatoSeleccionado, medioSeleccionado);
+                    break;
+                case 2:
+                    votacion.calcularCostoCampana();
+                    break;
+                case 3:
+                    votacion.vaciarUrnas();
+                    break;
+                case 4:
+                    votacion.saberResultados();
+                    break;
+                case 5:
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Opcion invalida");
+            }
+        }
     }
 }
--- src/Votacion.java
+++ src/Votacion.java
@@ -1,12 +1,57 @@
 import java.util.ArrayList;
 import java.util.List;
 public class Votacion {
-    private List<Candidato>candidatoes;
+    protected List<Candidato> candidatoes;
 
     public Votacion() {
         candidatoes = new ArrayList<>();
         candidatoes.add(new CandidatoInternet("Candidato 1"));
         candidatoes.add(new CandidatoRadio("Candidato 2"));
         candidatoes.add(new CandidatoTelevision("Candidato 3"));
+    }
+
+    public void votar(int candidatoSeleccionado, int medioSeleccionado) {
+        if (candidatoSeleccionado >= 0 && candidatoSeleccionado < candidatoes.size()) {
+            double costo = 0;
+            switch (medioSeleccionado) {
+                case 1:
+                    costo = 700000;
+                    break;
+                case 2:
+                    costo = 200000;
+                    break;
+                case 3:
+                    costo = 600000;
+                    break;
+            }
+            candidatoes.get(candidatoSeleccionado).votar(medioSeleccionado, costo);
+        }
+    }
+
+    public void calcularCostoCampana() {
+        double costoTotal = 0;
+        for (Candidato candidato : candidatoes) {
+            costoTotal += candidato.getCostoCampana();
+        }
+        double costoPromedio = costoTotal / candidatoes.size();
+        System.out.println("Costo promedio camapaña :" + costoPromedio);
+    }
+
+    public void vaciarUrnas() {
+        for (Candidato candidato : candidatoes) {
+            candidato.setVotos(0);
+            candidato.setCostoCampana(0);
+        }
+    }
+
+    public void saberResultados() {
+        int totalVotos = 0;
+        for (Candidato candidato : candidatoes) {
+            totalVotos += candidato.getVotos();
+        }
+        for (Candidato candidato : candidatoes) {
+            double porcentaje = (double) candidato.getVotos() / totalVotos * 100;
+            System.out.println(candidato.getNombre() + ":" + candidato.getVotos() + "votos(" + porcentaje + "%)");
+        }
     }
 }
